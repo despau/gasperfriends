69c3486278375b2e1ce3085688d5b4b5c08d34ec
raugdev24 <raugustin1049@gmail.com>
2020-03-10 15:13:26 -0400

    Updates

---
+++ precache-manifest.3957cfbf2567992b4f7222e29b1de672.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2173bef46c9e81d139619b52814c79c9",
+    "url": "/gasperfriends/index.html"
+  },
+  {
+    "revision": "2d24dc0de0e8dd8a8d30",
+    "url": "/gasperfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "debada64a2617254205c",
+    "url": "/gasperfriends/static/css/main.3d64cd0b.chunk.css"
+  },
+  {
+    "revision": "2d24dc0de0e8dd8a8d30",
+    "url": "/gasperfriends/static/js/2.773a8762.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gasperfriends/static/js/2.773a8762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "debada64a2617254205c",
+    "url": "/gasperfriends/static/js/main.5fd0b653.chunk.js"
+  },
+  {
+    "revision": "9bde30c3fbae4881557f",
+    "url": "/gasperfriends/static/js/runtime-main.2d3f5322.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/gasperfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
